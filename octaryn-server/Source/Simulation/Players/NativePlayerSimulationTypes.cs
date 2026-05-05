146d8a216152bcf313b8d3faf1a730e2679993dd
Zachary Rose <ZSG-Studios@users.noreply.github.com>
2026-05-05 05:52:05 -0400

    Split native player simulation interop types

---
+++ octaryn-server/Source/Simulation/Players/NativePlayerSimulationTypes.cs
@@ -0,0 +1,124 @@
+using System.Runtime.InteropServices;
+using Octaryn.Shared.Host;
+
+namespace Octaryn.Server.Simulation.Players;
+
+[StructLayout(LayoutKind.Sequential)]
+internal readonly struct NativeInput(
+    uint flags,
+    uint controller,
+    float moveX,
+    float moveY,
+    float moveZ,
+    float cameraX,
+    float cameraY,
+    float cameraZ,
+    float cameraPitch,
+    float cameraYaw,
+    int relativeMouse)
+{
+    public readonly uint Flags = flags;
+    public readonly uint Controller = controller;
+    public readonly float MoveX = moveX;
+    public readonly float MoveY = moveY;
+    public readonly float MoveZ = moveZ;
+    public readonly float CameraX = cameraX;
+    public readonly float CameraY = cameraY;
+    public readonly float CameraZ = cameraZ;
+    public readonly float CameraPitch = cameraPitch;
+    public readonly float CameraYaw = cameraYaw;
+    public readonly int RelativeMouse = relativeMouse;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+internal readonly struct NativeInputIntent(
+    int version,
+    ulong frameIndex,
+    double deltaSeconds,
+    NativeInput input)
+{
+    public readonly int Version = version;
+    public readonly ulong FrameIndex = frameIndex;
+    public readonly double DeltaSeconds = deltaSeconds;
+    public readonly NativeInput Input = input;
+
+    public HostFrameSnapshot ToFrameSnapshot()
+    {
+        return new HostFrameSnapshot(
+            new HostInputSnapshot(
+                HostInputSnapshot.VersionValue,
+                HostInputSnapshot.SizeValue,
+                Input.Flags,
+                Input.Controller,
+                Input.MoveX,
+                Input.MoveY,
+                Input.MoveZ,
+                Input.CameraX,
+                Input.CameraY,
+                Input.CameraZ,
+                Input.CameraPitch,
+                Input.CameraYaw,
+                Input.RelativeMouse),
+            new HostFrameTimingSnapshot(
+                HostFrameTimingSnapshot.VersionValue,
+                HostFrameTimingSnapshot.SizeValue,
+                FrameIndex,
+                DeltaSeconds));
+    }
+}
+
+[StructLayout(LayoutKind.Sequential)]
+internal struct NativeState(
+    float x,
+    float y,
+    float z,
+    float pitch,
+    float yaw,
+    float velocityX,
+    float velocityY,
+    float velocityZ,
+    uint isOnGround,
+    uint controlMode,
+    ushort selectedBlock)
+{
+    public float X = x;
+    public float Y = y;
+    public float Z = z;
+    public float Pitch = pitch;
+    public float Yaw = yaw;
+    public float VelocityX = velocityX;
+    public float VelocityY = velocityY;
+    public float VelocityZ = velocityZ;
+    public uint IsOnGround = isOnGround;
+    public uint ControlMode = controlMode;
+    public ushort SelectedBlock = selectedBlock;
+    public ushort Reserved;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+internal struct NativeSaveState(
+    float x,
+    float y,
+    float z,
+    float pitch,
+    float yaw,
+    ushort selectedBlock)
+{
+    public float X = x;
+    public float Y = y;
+    public float Z = z;
+    public float Pitch = pitch;
+    public float Yaw = yaw;
+    public ushort SelectedBlock = selectedBlock;
+    public ushort Reserved;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+internal struct NativeSpawnAlignment
+{
+    public uint Aligned;
+    public uint Adjusted;
+    public int SurfaceY;
+    public ushort SurfaceBlock;
+    public ushort Reserved;
+}
